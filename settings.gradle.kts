5aba207de43b9223be92c00e44c97753875a0cb8
Chris Turner <peavers@gmail.com>
2024-11-12 11:04:37 -0600

    feat: Implement gradle scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,15 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("com.gradle.enterprise") version "3.16.2"
 }
 
 rootProject.name = "hue-java-wrapper"
 include("lib")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
